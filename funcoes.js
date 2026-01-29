6785074cadb270bdd969d26521c43ee9e323c1b4
Nicole Moura <moura2007nick@gmail.com>
2026-01-29 10:12:17 -0300

    guardando valor da função

--- funcoes.js
+++ funcoes.js
@@ -4,3 +4,11 @@ function saudacao(nome) {  // nome é um parâmetro da função
 
 saudacao('Nicole')  // 'Nicole' é um argumento passado para a função
 saudacao('Claudia')
+
+function calcularDobro(numero) {
+    return numero * 2
+}
+
+const numeroDobrado = calcularDobro(4)
+
+console.log('o dobro de 4 é: ', numeroDobrado)
