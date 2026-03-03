39ef0321d9da1c9e22f39aad51aeabbb81d691fe
NickyM-hub <moura2007nick@gmail.com>
2026-03-03 09:22:16 -0300

    adicionando funções globais

--- funcoes.js
+++ funcoes.js
@@ -20,3 +20,14 @@ const calcularDobro = numero => numero * 2
 const numeroDobrado = calcularDobro(4)
 
 console.log('o dobro de 4 é: ', numeroDobrado)
+
+let contador = 0
+
+const id = setInterval( () => {
+    contador++
+    console.log('Tempo decorrido (em segundos)', contador)
+    if (contador == 10) {
+        clearInterval(id)
+    }
+        
+}, 1000)
