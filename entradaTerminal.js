0009df116b3b18d79285c3e5d17bcfbe01e963f0
NickyM-hub <moura2007nick@gmail.com>
2026-03-15 15:43:44 -0300

    terminando ativida entradaTerminal.js

--- entradaTerminal.js
+++ entradaTerminal.js
@@ -3,12 +3,20 @@ const readline= require('readline')
 const leitor = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-
 })
 
 leitor.question('Qual seu nome? ' ,(nome) => {
     console.log("Olá, ", nome)
     console.log("Boas vindas ao meu sistema")
 
-    leitor.close()
+    leitor.question('Qual a sua idade? ', (idade) => {
+        
+        if(idade >= 18){
+            console.log('Você pode tirar sua CNH')
+        } else {
+            console.log('Você ainda não pode tirar sua CNH')
+        }     
+        
+        leitor.close()
+    })
 })
